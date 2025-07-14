db308973c84ba020a8c207440a6eddeb754a1ed8
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-07-14 17:37:33 -0400

    Add PostUpdate and PostDelete views, add paths to urls.py, edit post_form.html, create a post_confirm_delete.html, add edit and delete buttons

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 # from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import PostUpdate, PostDelete
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
@@ -8,6 +9,11 @@ urlpatterns = [
     path('user_feed/', views.user_feed, name='user_feed'),
     path('posts/create/', views.PostCreate.as_view(), name='post-create'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:pk>/edit/', PostUpdate.as_view(), name='post_edit'),
+    path('posts/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),   
     path('profile.<int:user_id>/', views.profile_detail, name='profile_detail'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
+
+
+
 ]
